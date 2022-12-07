3dc70ab6046ff5aacdc10d7e60b2395e643c598b
Antoan Petrov <antoan999_kn@abv.bg>
2022-12-07 13:37:29 +0200

    add balance to the header and do some detail fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,4 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link to="/" class="navbar-brand">Trade stocks here!</router-link>
-
-
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
-                <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">End Day</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Save/Load Data
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Save</a>
-                        <a class="dropdown-item" href="#">Load</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</template>
-
-<template>
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -37,7 +10,7 @@
                     <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
                     <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
                 </ul>
-
+                <strong class="navbar-text navbar-right">Balance: {{ funds.toFixed(2) }}$</strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#">End Day</a></li>
                     <li class="dropdown">
@@ -53,3 +26,13 @@
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
-    <h1>
-        Home page!
-    </h1>
+    <div>
+        <h1>Trade ot View your Portfolio</h1>
+        <h6>You can Save and Load your Data</h6>
+        <h6>Click on 'End Day' to begin a New Day!</h6>
+        <hr>
+        <p>Your Balance: {{ funds }}</p>
+    </div>
 </template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
