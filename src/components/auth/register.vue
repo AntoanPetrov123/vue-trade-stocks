c4ba84402a0908d4abef1d3073e907208d17d823
Antoan Petrov <antoan999_kn@abv.bg>
2022-12-19 12:04:45 +0200

    add validators to register form

--- src/components/auth/register.vue
+++ src/components/auth/register.vue
@@ -2,38 +2,50 @@
     <div id="signup">
         <div class="signup-form">
             <form @submit.prevent="onSubmit">
-                <div class="input">
+                <div class="input" :class="{ invalid: $v.username.$error }">
                     <label for="username">Username</label>
-                    <input type="text" id="username" v-model="username">
+                    <input type="text" id="username" @touch="$v.username.$touch()" v-model="username">
+                    <p v-if="!$v.username.required">Username must be not empty</p>
                 </div>
-                <div class="input">
+                <div class="input" :class="{ invalid: $v.email.$error }">
                     <label for="email">Mail</label>
-                    <input type="email" id="email" v-model="email">
+                    <!-- input="$v" give us access to validators / reserved word -->
+                    <input type="email" id="email" @blur="$v.email.$touch()" v-model="email">
+                    <p v-if="!$v.email.email">Email is invalid</p>
                 </div>
-                <div class="input">
+                <div class="input" :class="{ invalid: $v.age.$error }">
                     <label for="age">Your Age</label>
-                    <input type="number" id="age" v-model.number="age">
+                    <input type="number" id="age" @blur="$v.age.$touch()" v-model.number="age">
+                    <!-- $v.age.$params.minVal.min get params passed to a function, in our case it is 18 -->
+                    <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} years old.</p>
                 </div>
-                <div class="input">
+                <div class="input" :class="{ invalid: $v.password.$error }">
                     <label for="password">Password</label>
-                    <input type="password" id="password" v-model="password">
+                    <input type="password" id="password" @blur="$v.password.$touch()" v-model="password">
+                    <p v-if="!$v.password.minLen">Password must be at least {{ $v.password.$params.minLen.min }}
+                        characters.
+                    </p>
                 </div>
-                <div class="input">
+                <div class="input" :class="{ invalid: $v.confirmPassword.$error }">
                     <label for="confirm-password">Confirm Password</label>
-                    <input type="password" id="confirm-password" v-model="confirmPassword">
+                    <input type="password" id="confirm-password" @blur="$v.confirmPassword.$touch()"
+                        v-model="confirmPassword">
+                    <p v-if="!$v.confirmPassword.sameAs">Passwords must be equal.</p>
                 </div>
                 <div class="input">
                     <label for="country">Country</label>
                     <select id="country" v-model="country">
                         <option value="usa">USA</option>
-                        <option value="india">India</option>
+                        <option value="bulgaria">Bulgaria</option>
                         <option value="uk">UK</option>
                         <option value="germany">Germany</option>
                     </select>
                 </div>
-                <div class="input">
+                <div class="input" :class="{ invalid: $v.balance.$error }">
                     <label for="balance">Balance</label>
-                    <input type="number" id="balance" v-model="balance">
+                    <input type="number" id="balance" @blur="$v.balance.$touch()" v-model="balance">
+                    <p v-if="!$v.balance.minVal">You have deposit at least {{ $v.balance.$params.minVal.min }} $.</p>
+                    <p v-if="!$v.balance.maxVal">You have deposit at last {{ $v.balance.$params.maxVal.max }} $.</p>
                 </div>
                 <!-- <div class="hobbies">
                     <h3>Add some Hobbies</h3>
@@ -46,8 +58,8 @@
                         </div>
                     </div>
                 </div> -->
-                <div class="input inline">
-                    <input type="checkbox" id="terms" v-model="terms">
+                <div class="input inline" :class="{ invalid: $v.terms.$valid }">
+                    <input type="checkbox" id="terms" @change="$v.terms.$touch()" v-model="terms">
                     <label for="terms">Accept Terms of Use</label>
                 </div>
                 <div class="submit">
@@ -59,6 +71,8 @@
 </template>
   
 <script>
+import { required, email, numeric, minValue, minLength, sameAs, requiredUnless, maxValue } from 'vuelidate/lib/validators';
+
 export default {
     data() {
         return {
@@ -71,6 +85,41 @@ export default {
             balance: null,
             // hobbyInputs: [],
             terms: false
+        }
+    },
+    validations: {
+        username: {
+            required
+        },
+        email: {
+            required,
+            email
+        },
+        age: {
+            required,
+            numeric,
+            minVal: minValue(18)
+        },
+        password: {
+            required,
+            minLen: minLength(6)
+        },
+        confirmPassword: {
+            // sameAs: sameAs('password')
+            sameAs: sameAs(vm => {
+                return vm.password;
+            })
+        },
+        terms: {
+            required: requiredUnless(vm => {
+                return vm.country === 'bulgaria';
+            })
+        },
+        balance: {
+            required,
+            numeric,
+            minVal: minValue(100),
+            maxVal: maxValue(10000)
         }
     },
     methods: {
@@ -142,6 +191,15 @@ export default {
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
+}
+
+.input.invalid label {
+    color: red;
+}
+
+.input.invalid input {
+    border: 1px solid red;
+    background-color: #ee6e86;
 }
 
 .input select {
